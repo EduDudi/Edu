5b9c955ca541680567b986b154393cc0ebc3f24f
Eduardo <141647010+EduDudi@users.noreply.github.com>
2023-10-23 08:40:58 -0400

    musica2.js

--- musica2.js
+++ musica2.js
@@ -2,51 +2,52 @@ export default [
     {      
       src: "./musica/Artic.mp3",
       name: "RU mine - Artic Monkeys",
-      
+      image: "./img/artic.jpg"
     },
     {
       src: "./musica/borns.mp3",
       name: "Past Lives - BORNS",
-     
+     image: "./img/borns.jpg"
     },
     {
       src: "./musica/doja.mp3",
       name: "Need to know - Doja Cat",
-     
+     image: "./img/doja.jpg"
     },
     {
       src: "./musica/euevc.mp3",
       name: "you & me - Disclosure",
-      
+      image: "./img/disclosure.jpg"
     },
     {
       src: "./musica/girl.mp3",
       name: "Promiscuos girl",
-      
+      image: "./img/girl.jpg"
     },
     {
       src: "./musica/lana.mp3",
       name: "Let the lights - Lana Del Rey",
-      
+      image: "./img/lana.webp"
     },
     {
       src: "./musica/les.mp3",
       name: "Les - Childish Gambino",
-      
+      image: "./img/les.jpg"
     },
     {
       src: "./musica/nadanovo.mp3",
       name: "Nothing new - Rio Romeo",
+      name: "Nothing image: "./img/rio.jpg"new - Rio Romeo",
       
     },
     {
       src: "./musica/sunshine.mp3",
       name: "Sunshine - Delacruz",
-      
+      image: "./img/dellacruz.webp"
     },
     {
       src: "./musica/vizinhos.mp3",
       name: "Leaving in Tonight - The neightborhood",
-      
+      image: "./img/vizinhos.jpg"
     },
   ];
